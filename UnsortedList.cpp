669a4cf3ceae7a1d3521d8463e998a6af9b2d838
Matthew Cawley <matthewisaac.cawley@gmail.com>
2025-09-20 23:44:45 -0700

    followed lecture sep 2 complete

--- UnsortedList.cpp
+++ UnsortedList.cpp
@@ -9,17 +9,31 @@ UnsortedList<T>::UnsortedList() {
 
 template<class T>
 void UnsortedList<T>::MakeEmpty() {
-
+    length = 0;
+    while (head != nullptr)
+    {
+        Node *temp = head;
+        head = head->next;
+        delete temp;
+    }
 }
 
 template<class T>
 bool UnsortedList<T>::IsFull() const {
+    try
+    {
+        Node *testNode = new Node(); // try to make a test node
+        delete testNode; // we're good... delete test node
+    } catch (std::bad_alloc &e)
+    {
+        return true;
+    }
     return false;
 }
 
 template<class T>
 int UnsortedList<T>::GetLength() const {
-    return 0;
+    return length;
 }
 
 template<class T>
@@ -48,7 +62,7 @@ void UnsortedList<T>::AddItem(T item) {
 
     Node *insertedNode = new Node;
     insertedNode->value = item;
-    insertedNode->next - nullptr;
+    insertedNode->next = nullptr;
 
     head = insertedNode;
     ++length;
@@ -58,23 +72,37 @@ void UnsortedList<T>::AddItem(T item) {
 template<class T>
 void UnsortedList<T>::DeleteItem(T item) {
     Node* currN = head;
+    Node* prevN = nullptr; // no previous
 
     while (currN != nullptr)
     {
-        if (currN->value == someItem)
+        if (currN->value == item)
         {
-
+            if (prevN != nullptr) // not beginning
+            {
+                // handle middle case
+                prevN->next = currN->next;
+            } else
+            {
+                head = currN->next;
+            }
+            delete currN;
+            --length;
+            return;
         }
+        prevN = currN;
         currN = currN->next;
     }
 }
 
 template<class T>
 void UnsortedList<T>::ResetIterator() {
-
+    currPos = head;
 }
 
 template<class T>
 T UnsortedList<T>::GetNextItem() {
+    T currVal = currPos->value;
+    currPos = currPos->next;
     return T();
 }
